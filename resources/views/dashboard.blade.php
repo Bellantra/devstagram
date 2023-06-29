f2d7f7750237ef425c69d4bfa5831826ae35f8ac
Bellantra <yamilapaez2@hotmail.com>
2023-06-29 18:18:42 +0200

    section 18 finished, post added to dashboard and pagination

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -30,4 +30,27 @@
       </div>
     </div>
 </div>
+
+<section class="container mx-auto mt-10">
+   <h2 class="text-4xl text-center font-black my-10">Posts</h2>
+
+   @if ($posts ->count())   
+
+   <div class="grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
+      @foreach ( $posts as $post )
+      <div>
+         <a href="">
+            <img src="{{ asset('uploads') . '/' . $post->image}}" alt="post image {{$post->title}}">
+         </a>
+      </div>      
+   @endforeach  
+   </div>
+   <div class="my-10 mx-20">
+      {{$posts->links('pagination::tailwind')}}
+   </div>
+
+   @else
+      <p class="text-gray-600 uppercase text-sm text-center font-bold ">There is no Posts</p>
+  @endif
+</section>
 @endsection
